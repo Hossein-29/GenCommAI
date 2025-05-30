d121c3dcba5b6644965ad25a064d15855e092c07
tahaakbarialvanagh <tahaakbarialvanagh@gmail.com>
2025-05-30 11:33:08 +0330

    Added youtube searcher

--- app/services/search_youtube_videos.py
+++ app/services/search_youtube_videos.py
@@ -1,12 +1,13 @@
 from openai import OpenAI
+from pydantic import BaseModel
 import os
 
 client = OpenAI(base_url="https://api.metisai.ir/openai/v1", api_key=os.getenv("OPENAI_API_KEY"))
 
 response = client.responses.create(
-    model="gpt-4.1",
+    model="gpt-4.1-mini",
     tools=[{ "type": "web_search_preview" }],
-    input="What was a positive news story from today?",
+    input="Find youtube videos about the following product give the results as a json list of youtube urls containing only:\niphone 16 pro max 256 gb",
 )
 
-print(response)
+print(response.output_text)
