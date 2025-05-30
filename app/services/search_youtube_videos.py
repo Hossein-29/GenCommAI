b0a2a4911b82d2ebf586b6dd826fbcea4e70b03f
Moein Madadi <moeinmadadi2002@gmail.com>
2025-05-30 11:36:24 +0330

    test

--- app/services/search_youtube_videos.py
+++ app/services/search_youtube_videos.py
@@ -7,7 +7,18 @@ client = OpenAI(base_url="https://api.metisai.ir/openai/v1", api_key=os.getenv("
 response = client.responses.create(
     model="gpt-4.1-mini",
     tools=[{ "type": "web_search_preview" }],
-    input="Find youtube videos about the following product give the results as a json list of youtube urls containing only:\niphone 16 pro max 256 gb",
+    input="""
+        Find YouTube videos about the following product and return the results as a JSON list of YouTube URLs. Each URL should be a string and the JSON object should only contain the URLs of videos that are related to "iphone 16 pro max 256 gb". The output should be structured like this:
+
+        {
+        "videos": [
+            "https://www.youtube.com/watch?v=video_id_1",
+            "https://www.youtube.com/watch?v=video_id_2",
+            "https://www.youtube.com/watch?v=video_id_3"
+        ]
+        }
+
+        """,
 )
 
 print(response.output_text)
